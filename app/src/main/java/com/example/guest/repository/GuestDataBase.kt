88e5e4225febc8d41d58ee4ebf3be5e7b530cbb2
lucasgomes14 <lucazmatehus14@gmail.com>
2024-10-17 16:53:01 -0300

    Room: Database layer

--- app/src/main/java/com/example/guest/repository/GuestDataBase.kt
+++ app/src/main/java/com/example/guest/repository/GuestDataBase.kt
@@ -3,9 +3,15 @@ package com.example.guest.repository
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.guest.constants.DataBaseConstants
+import com.example.guest.model.GuestModel
 
-class GuestDataBase(context: Context) : SQLiteOpenHelper(context, NAME, null, VERSION) {
+/*class GuestDataBase(context: Context) : SQLiteOpenHelper(context, NAME, null, VERSION) {
     companion object {
         private const val NAME = "guestdb"
         private const val VERSION = 1
@@ -19,4 +25,31 @@ class GuestDataBase(context: Context) : SQLiteOpenHelper(context, NAME, null, VE
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {}
+}
+*/
+
+@Database(entities = [GuestModel::class], version = 1)
+abstract class GuestDataBase : RoomDatabase() {
+    companion object {
+        private lateinit var INSTANCE: GuestDataBase
+
+        fun getDatabase(context: Context): GuestDataBase {
+            if (!::INSTANCE.isInitialized) {
+                synchronized(GuestDataBase::class) {
+                    INSTANCE = Room.databaseBuilder(context, GuestDataBase::class.java, "guestdb")
+                        .addMigrations(MIGRATION_1_2)
+                        .allowMainThreadQueries()
+                        .build()
+                }
+            }
+            return INSTANCE
+        }
+
+        private val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+
+            }
+
+        }
+    }
 }
